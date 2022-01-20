af8a3166c31f46a71e77bddd2b6093c9a88ef969
MaxXSoft <x@MaxXSoft.net>
2022-01-20 21:24:07 +0800

    (irgen) added some details

--- src/irgen.rs
+++ src/irgen.rs
@@ -1,5 +1,5 @@
 use crate::ast::*;
-use koopa::ir::{Function, Program, Value};
+use koopa::ir::{Function, Program};
 use std::collections::HashMap;
 use std::fmt;
 
@@ -13,26 +13,87 @@ pub fn generate_program(comp_unit: &CompUnit) -> Result<Program> {
 /// Scopes, including all values, constants and functions definitions.
 struct Scopes<'ast> {
   vals: Vec<HashMap<&'ast str, Value>>,
-  consts: Vec<HashMap<&'ast str, i32>>,
   funcs: HashMap<&'ast str, Function>,
 }
 
 impl<'ast> Scopes<'ast> {
+  /// Creates a new `Scopes`.
   fn new() -> Self {
     Self {
       vals: vec![HashMap::new()],
-      consts: vec![HashMap::new()],
       funcs: HashMap::new(),
     }
   }
+
+  /// Returns `true` if is currently in global scope.
+  fn is_global(&self) -> bool {
+    self.vals.len() == 1
+  }
+
+  /// Inserts a new value to the current scope.
+  fn new_value(&mut self, id: &'ast str, value: Value) -> Result<()> {
+    let is_global = self.is_global();
+    let cur = self.vals.last_mut().unwrap();
+    if cur.contains_key(id) || (is_global && self.funcs.contains_key(id)) {
+      Err(Error::DuplicatedDef)
+    } else {
+      cur.insert(id, value);
+      Ok(())
+    }
+  }
+
+  /// Returns the value by the given identifier.
+  fn value(&self, id: &str) -> Result<&Value> {
+    let mut cur = self.vals.len() as i32 - 1;
+    while cur >= 0 {
+      if let Some(value) = self.vals[cur as usize].get(id) {
+        return Ok(value);
+      }
+      cur -= 1;
+    }
+    Err(Error::SymbolNotFound)
+  }
+
+  /// Inserts a new function to the current scope.
+  fn new_func(&mut self, id: &'ast str, func: Function) -> Result<()> {
+    if self.funcs.contains_key(id) || self.vals.first().unwrap().contains_key(id) {
+      Err(Error::DuplicatedDef)
+    } else {
+      self.funcs.insert(id, func);
+      Ok(())
+    }
+  }
+
+  /// Returns the function by the given identifier.
+  fn func(&self, id: &str) -> Result<Function> {
+    self.funcs.get(id).copied().ok_or(Error::SymbolNotFound)
+  }
+}
+
+/// A value.
+enum Value {
+  /// Koopa IR value.
+  Value(koopa::ir::Value),
+  /// Constant integer.
+  Int(i32),
+  /// Constant zeros (zeroed array).
+  Zeros,
+  /// Constant array.
+  Array(Vec<i32>),
 }
 
 /// Error returned by IR generator.
-pub struct Error();
+pub enum Error {
+  DuplicatedDef,
+  SymbolNotFound,
+}
 
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    todo!()
+    match self {
+      Self::DuplicatedDef => write!(f, "duplicated symbol definition"),
+      Self::SymbolNotFound => write!(f, "symbol not found"),
+    }
   }
 }
 
@@ -82,7 +143,10 @@ impl<'ast> GenerateProgram<'ast> for ConstDecl {
   type Out = ();
 
   fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
-    todo!()
+    for def in &self.defs {
+      def.generate(program, scopes)?;
+    }
+    Ok(())
   }
 }
 
@@ -95,7 +159,7 @@ impl<'ast> GenerateProgram<'ast> for ConstDef {
 }
 
 impl<'ast> GenerateProgram<'ast> for ConstInitVal {
-  type Out = ();
+  type Out = Value;
 
   fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
     todo!()
