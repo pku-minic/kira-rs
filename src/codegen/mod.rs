92a8e3bb4da204538bb8240f93f3a9b78573f943
MaxXSoft <x@MaxXSoft.net>
2022-01-27 13:24:13 +0800

    (codegen) added more implementations

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -5,7 +5,7 @@ use builder::AsmBuilder;
 use func::FunctionInfo;
 use koopa::ir::entities::ValueData;
 use koopa::ir::values::*;
-use koopa::ir::{BasicBlock, Function, FunctionData, Program, Type, Value, ValueKind};
+use koopa::ir::{BasicBlock, Function, FunctionData, Program, Type, TypeKind, Value, ValueKind};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Result, Write};
@@ -48,53 +48,56 @@ impl<'p> ProgramInfo<'p> {
   }
 }
 
-/// A helper struct for checking if a value requires slot allocaiton.
-struct AllocChecker<'f> {
-  func: &'f FunctionData,
-  cached: HashMap<*const ValueData, bool>,
-}
-
-impl<'f> AllocChecker<'f> {
-  fn new(func: &'f FunctionData) -> Self {
-    Self {
-      func,
-      cached: HashMap::new(),
-    }
-  }
-
-  /// Returns `true` if the given value should be allocated.
-  fn should_alloc(&mut self, value: &ValueData) -> bool {
-    if let Some(alloc) = self.cached.get(&(value as *const ValueData)) {
-      return *alloc;
-    } else {
-      let alloc =
-        if !value.kind().is_local_inst() || value.used_by().is_empty() || value.ty().is_unit() {
-          false
-        } else {
-          match value.kind() {
-            ValueKind::Load(_) => false,
-            _ => {
-              value
-                .kind()
-                .value_uses()
-                .filter(|v| self.should_alloc(self.func.dfg().value(*v)))
-                .count()
-                > 1
-            }
-          }
-        };
-      self.cached.insert(value, alloc);
-      alloc
-    }
-  }
-}
-
 /// A global/local value.
 enum AsmValue<'i> {
   Global(&'i str),
   Local(usize),
   Const(i32),
-  Temp,
+}
+
+/// Returns the assembly value of the given value data.
+macro_rules! asm_value {
+  ($info:expr, $v:expr) => {
+    AsmValue::Local(cur_func!($info).slot_offset($v))
+  };
+}
+
+impl<'i> AsmValue<'i> {
+  /// Writes the assembly value to the given register.
+  fn write_to(&self, f: &mut File, reg: &'static str) -> Result<()> {
+    let mut builder = AsmBuilder::new(f, reg);
+    match self {
+      Self::Global(symbol) => {
+        builder.la(reg, symbol)?;
+        builder.lw(reg, reg, 0)
+      }
+      Self::Local(offset) => builder.lw(reg, "sp", *offset as i32),
+      Self::Const(num) => builder.li(reg, *num),
+    }
+  }
+
+  /// Writes the address of assembly value to the give register.
+  fn write_addr_to(&self, f: &mut File, reg: &'static str) -> Result<()> {
+    let mut builder = AsmBuilder::new(f, reg);
+    match self {
+      Self::Global(symbol) => builder.la(reg, symbol),
+      Self::Local(offset) => builder.addi(reg, "sp", *offset as i32),
+      Self::Const(_) => unreachable!(),
+    }
+  }
+
+  /// Reads the value of the given register to the assembly value.
+  fn read_from(&self, f: &mut File, reg: &'static str, temp: &'static str) -> Result<()> {
+    let mut builder = AsmBuilder::new(f, temp);
+    match self {
+      Self::Global(symbol) => {
+        builder.la(temp, symbol)?;
+        builder.sw(reg, temp, 0)
+      }
+      Self::Local(offset) => builder.sw(reg, "sp", *offset as i32),
+      Self::Const(_) => unreachable!(),
+    }
+  }
 }
 
 /// Trait for generating RISC-V assembly.
@@ -159,10 +162,9 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for FunctionData {
     }
     // allocation stack slots and log argument number
     let func = cur_func_mut!(info);
-    let mut checker = AllocChecker::new(self);
     for value in self.dfg().values().values() {
       // allocate stack slot
-      if checker.should_alloc(value) {
+      if value.kind().is_local_inst() && !value.used_by().is_empty() {
         func.alloc_slot(value);
       }
       // log argument number
@@ -177,7 +179,7 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for FunctionData {
       func.log_bb_name(bb, data.name());
     }
     // generate prologue
-    AsmBuilder::new(f, "t0").prologue(self.name(), &func)?;
+    AsmBuilder::new(f, "t0").prologue(self.name(), func)?;
     // generate instructions in basic blocks
     for (bb, node) in self.layout().bbs() {
       let name = bb.generate(f, info)?;
@@ -186,8 +188,7 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for FunctionData {
         self.dfg().value(inst).generate(f, info)?;
       }
     }
-    // generate epilogue
-    AsmBuilder::new(f, "t0").epilogue(&info.cur_func.take().unwrap())
+    Ok(())
   }
 }
 
@@ -208,12 +209,9 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for Value {
     } else {
       let func = cur_func!(info);
       let value = info.program.func(func.func()).dfg().value(*self);
-      Ok(if let ValueKind::Integer(i) = value.kind() {
-        AsmValue::Const(i.value())
-      } else if let Some(offset) = func.slot_offset(value) {
-        AsmValue::Local(offset)
-      } else {
-        AsmValue::Temp
+      Ok(match value.kind() {
+        ValueKind::Integer(i) => AsmValue::Const(i.value()),
+        _ => AsmValue::Local(func.slot_offset(value)),
       })
     }
   }
@@ -228,11 +226,11 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for ValueData {
       ValueKind::ZeroInit(v) => v.generate(f, info, self),
       ValueKind::Aggregate(v) => v.generate(f, info),
       ValueKind::GlobalAlloc(v) => v.generate(f, info),
-      ValueKind::Load(v) => v.generate(f, info),
+      ValueKind::Load(v) => v.generate(f, info, self),
       ValueKind::Store(v) => v.generate(f, info),
-      ValueKind::GetPtr(v) => v.generate(f, info),
-      ValueKind::GetElemPtr(v) => v.generate(f, info),
-      ValueKind::Binary(v) => v.generate(f, info),
+      ValueKind::GetPtr(v) => v.generate(f, info, self),
+      ValueKind::GetElemPtr(v) => v.generate(f, info, self),
+      ValueKind::Binary(v) => v.generate(f, info, self),
       ValueKind::Branch(v) => v.generate(f, info),
       ValueKind::Jump(v) => v.generate(f, info),
       ValueKind::Call(v) => v.generate(f, info),
@@ -277,11 +275,12 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for GlobalAlloc {
   }
 }
 
-impl<'p, 'i> GenerateToAsm<'p, 'i> for Load {
+impl<'p, 'i> GenerateValueToAsm<'p, 'i> for Load {
   type Out = ();
 
-  fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
-    todo!()
+  fn generate(&self, f: &mut File, info: &mut ProgramInfo, v: &ValueData) -> Result<Self::Out> {
+    self.src().generate(f, info)?.write_to(f, "t0")?;
+    asm_value!(info, v).read_from(f, "t0", "t1")
   }
 }
 
@@ -289,30 +288,52 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for Store {
   type Out = ();
 
   fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
-    todo!()
+    self.value().generate(f, info)?.write_to(f, "t0")?;
+    self.dest().generate(f, info)?.read_from(f, "t0", "t1")
   }
 }
 
-impl<'p, 'i> GenerateToAsm<'p, 'i> for GetPtr {
+impl<'p, 'i> GenerateValueToAsm<'p, 'i> for GetPtr {
   type Out = ();
 
-  fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
-    todo!()
+  fn generate(&self, f: &mut File, info: &mut ProgramInfo, v: &ValueData) -> Result<Self::Out> {
+    self.src().generate(f, info)?.write_addr_to(f, "t0")?;
+    self.index().generate(f, info)?.write_to(f, "t1")?;
+    let size = match v.ty().kind() {
+      TypeKind::Pointer(base) => base.size(),
+      _ => unreachable!(),
+    };
+    let mut builder = AsmBuilder::new(f, "t2");
+    builder.muli("t1", "t1", size as i32)?;
+    builder.add("t0", "t0", "t1")?;
+    asm_value!(info, v).read_from(f, "t0", "t1")
   }
 }
 
-impl<'p, 'i> GenerateToAsm<'p, 'i> for GetElemPtr {
+impl<'p, 'i> GenerateValueToAsm<'p, 'i> for GetElemPtr {
   type Out = ();
 
-  fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
-    todo!()
+  fn generate(&self, f: &mut File, info: &mut ProgramInfo, v: &ValueData) -> Result<Self::Out> {
+    self.src().generate(f, info)?.write_addr_to(f, "t0")?;
+    self.index().generate(f, info)?.write_to(f, "t1")?;
+    let size = match v.ty().kind() {
+      TypeKind::Pointer(base) => match base.kind() {
+        TypeKind::Array(base, _) => base.size(),
+        _ => unreachable!(),
+      },
+      _ => unreachable!(),
+    };
+    let mut builder = AsmBuilder::new(f, "t2");
+    builder.muli("t1", "t1", size as i32)?;
+    builder.add("t0", "t0", "t1")?;
+    asm_value!(info, v).read_from(f, "t0", "t1")
   }
 }
 
-impl<'p, 'i> GenerateToAsm<'p, 'i> for Binary {
+impl<'p, 'i> GenerateValueToAsm<'p, 'i> for Binary {
   type Out = ();
 
-  fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
+  fn generate(&self, f: &mut File, info: &mut ProgramInfo, v: &ValueData) -> Result<Self::Out> {
     todo!()
   }
 }
@@ -345,6 +366,9 @@ impl<'p, 'i> GenerateToAsm<'p, 'i> for Return {
   type Out = ();
 
   fn generate(&self, f: &mut File, info: &mut ProgramInfo) -> Result<Self::Out> {
-    todo!()
+    if let Some(value) = self.value() {
+      value.generate(f, info)?.write_to(f, "a0")?;
+    }
+    AsmBuilder::new(f, "t0").epilogue(cur_func!(info))
   }
 }
