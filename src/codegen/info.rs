11ad4c95551926b697e39c3c50ef11541bb2a341
MaxXSoft <x@MaxXSoft.net>
2022-01-27 17:58:42 +0800

    (info) added some comments

--- src/codegen/info.rs
+++ src/codegen/info.rs
@@ -35,26 +35,32 @@ impl<'p> ProgramInfo<'p> {
     }
   }
 
+  /// Returns a reference to the program.
   pub fn program(&self) -> &'p Program {
     self.program
   }
 
+  /// Returns the name of the given global value.
   pub fn value(&self, value: Value) -> &str {
     self.values.get(&value).unwrap()
   }
 
+  /// Inserts a new global value name.
   pub fn insert_value(&mut self, value: Value, name: String) {
     self.values.insert(value, name);
   }
 
+  /// Returns a reference to the current function information.
   pub fn cur_func(&self) -> Option<&FunctionInfo> {
     self.cur_func.as_ref()
   }
 
+  /// Returns a mutable reference to the current function information.
   pub fn cur_func_mut(&mut self) -> Option<&mut FunctionInfo> {
     self.cur_func.as_mut()
   }
 
+  /// Sets the current function information.
   pub fn set_cur_func(&mut self, func: FunctionInfo) {
     self.cur_func = Some(func);
   }
